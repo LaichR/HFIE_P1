d54c025fd5ec0d7b843003574a40899a0eb5683b
LaichR <46196385+LaichR@users.noreply.github.com>
2020-10-28 16:10:30 +0100

    aufgaben 2b und 2c

--- MoreExamples/MoreExamples.c
+++ MoreExamples/MoreExamples.c
@@ -5,16 +5,52 @@
 #include <stdint.h>
 #include <string.h>
 
-uint8_t CheckIfPalindrom(const char* palindrom);
+//
+// enums are integer values that are used with a name
+// in c the enums-values are treated as compile time constants with global scope
+//
+
+typedef enum
+{
+    False=0,
+    True=1
+}Bool;
+
+/**
+* Prototype of a function; a prototype is required if
+* the function definition is written after its first use. 
+* There are cases, where we could do without by just moving the the definition to the beginning of a file.
+* But there are also cases, where we cannot order the functions properly.
+* - This is the case when we use libraries
+* - This may be the case when we have recursive calls A() -> B() and B() -> A()
+* 
+* Therefore its common practice to place the prototype of the functions separate to a header (for public functions)
+* or to the beginning of a c-Source
+*/
+ 
+Bool CheckIfPalindrom(const char* palindrom);
+
+int32_t FirstIndexOf1(const char* text, char ch);
+int32_t FirstIndexOf2(const char* text, char ch);
+
+char* ToLowerCase(char* text);
 
 int main()
 {
     printf("hello world\n");
     uint8_t isPalindrom = CheckIfPalindrom("anna");
     printf("it was a plindrom %i", isPalindrom);
+
+    int32_t index1 = FirstIndexOf1("hello", 'l');
+    int32_t index2 = FirstIndexOf2("hello", 'l');
+
+    char buffer[128];
+    strcpy_s(buffer, sizeof buffer, "ALIBABA UND DIE 40 RAEUBER");
+
+    printf("to lowercase: %s", ToLowerCase(buffer));
 }
 
-uint8_t CheckIfPalindrom(const char* palindrom)
+Bool CheckIfPalindrom(const char* palindrom)
 {
     uint32_t low = 0;
     uint32_t high = strnlen_s(palindrom, 128)-1;
@@ -25,3 +61,48 @@ uint8_t CheckIfPalindrom(const char* palindrom)
     }
     return low >= high;
 }
+
+/**
+* 
+*/
+int32_t FirstIndexOf1(const char* text, char ch)
+{
+    int32_t i = 0;
+    char curChar = text[i];
+    while (curChar != '\0' )
+    {
+        if (curChar == ch) return i;
+        i++;
+        curChar = text[i];
+    }
+    return -1;
+}
+
+/**
+* hantieren mit Zeigern
+*/
+int32_t FirstIndexOf2(const char* text, char ch)
+{
+    const char* p = text;
+    while (*p != '\0')
+    {
+        if (*p == ch) return (p - text);
+        p++;
+    }
+    return -1;
+}
+
+
+char *ToLowerCase(char* text)
+{
+    char* p = text;
+    for (; *p; p++)
+    {
+        if ((*p >= 'A' && *p <= 'Z' ))
+        {
+            *p |= 0x20;
+        }
+    }
+    return text;
+}
+
